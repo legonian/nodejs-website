2aaf2f109a4c168ac5f36764ac2e5a777bfb26f2
Oleh <oleg.2014@gmail.com>
2020-09-12 21:49:55 +0300

    Add tabs on login/signup window, to close window on click outside

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,31 +1,49 @@
-let sign_up_btn = document.getElementById('sign_up')
-let log_in_btn = document.getElementById('log_in')
-let submit_btn = document.getElementById('submit')
+const signup_btn = document.getElementById('sign_up')
+const login_btn = document.getElementById('log_in')
 
-let modal_close_btn = document.getElementsByClassName('modal-close')[0]
-let modal_window = document.getElementsByClassName('modal-background')[0]
-let login_form = document.getElementsByClassName('login-form')[0]
-let on_sign_up = document.getElementsByClassName('on_sign_up')
+const login_tab_btn = document.getElementById('login_tab')
+const signup_tab_btn = document.getElementById('signup_tab')
 
-log_in_btn.onclick = function () {
-    on_sign_up[0].style.display = 'none'
-    on_sign_up[0].required = false
-    on_sign_up[1].style.display = 'none'
-    login_form.action = '/login'
-    submit_btn.value = 'Log In'
-    modal_window.style.display = 'block'
+const login_tab = document.getElementsByClassName('login')[0]
+const signup_tab = document.getElementsByClassName('signup')[0]
+
+const modal_window = document.getElementsByClassName('modal-background')[0]
+const modal_close_btn = document.getElementsByClassName('modal-close')[0]
+
+function show_signup_tab() {
+  signup_tab.style.display = "block"
+  login_tab.style.display = "none"
+
+  signup_tab_btn.classList.add('active')
+  login_tab_btn.classList.remove('active')
+}
+function show_login_tab() {
+  login_tab.style.display = "block"
+  signup_tab.style.display = "none"
+
+  login_tab_btn.classList.add('active')
+  signup_tab_btn.classList.remove('active')
 }
 
-sign_up_btn.onclick = function () {
-    on_sign_up[0].style.display = 'block'
-    on_sign_up[0].required = true
-    on_sign_up[1].style.display = 'block'
+signup_btn.onclick = function () {
+  modal_window.style.display = 'block'
+  show_signup_tab()
+}
+login_btn.onclick = function () {
+  modal_window.style.display = 'block'
+  show_login_tab()
+}
 
-    login_form.action = '/signup'
-    submit_btn.value = 'Sign Up'
-    modal_window.style.display = 'block'
+modal_window.onclick = function(event) {
+  if (event.target == modal_window) {
+    modal_window.style.display = "none"
+  }
 }
 
 modal_close_btn.onclick = function () {
-    modal_window.style.display = 'none'
+  modal_window.style.display = 'none'
 }
+
+login_tab_btn.onclick = function () {show_login_tab()}
+signup_tab_btn.onclick = function () {show_signup_tab()}
+
