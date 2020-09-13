d3aed6ba9f58390be0b7fd085eda92faf6771851
Oleh <oleg.2014@gmail.com>
2020-09-13 21:11:05 +0300

    fix minor bug

--- middleware/auth_user.js
+++ middleware/auth_user.js
@@ -1,24 +1,29 @@
 const User = require('../models/user')
 
-function auth_user (option){
+async function f1 () {
+  if(option == 'login'){
+    const temp_user = new User({name: req.body.username,
+                                      pass: req.body.password})
+    const db_user = await temp_user.auth()
+  }else if(option == 'signup'){
+    const temp_user = new User({name: req.body.username, 
+                                pass: req.body.password, 
+                                first_name: req.body.first_name})
+    const db_user = await temp_user.sign_up()
+  }else{
+    const db_user = null
+  }
+  return db_user
+}
+
+function auth_user (option) {
   return async function (req, res, next) {
     try {
       if(!req.captcha) {
         req.session.error = 'Authentication failed, please check captcha.'
         next()
       }else{
-        if(option == 'login'){
-          const temp_user = new User({name: req.body.username,
-                                            pass: req.body.password})
-          const db_user = await temp_user.auth()
-        }else if(option == 'signup'){
-          const temp_user = new User({name: req.body.username, 
-                                      pass: req.body.password, 
-                                      first_name: req.body.first_name})
-          const db_user = await temp_user.sign_up()
-        }else{
-          const db_user = null
-        }
+        const db_user = await f1()
 
         if(db_user){
           req.session.regenerate(function(){
