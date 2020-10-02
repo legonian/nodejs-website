5ce5c6fd64439dd27c312e35456631c9c21756ae
Oleh <oleg.2014@gmail.com>
2020-10-02 04:01:28 +0300

    add tempates script to test only frontend

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,6 @@ function rand() {
 }
 
 router.get('/', function(req, res) {
-  res.locals.topics = JSON.stringify([
-    { suptitle : "Some very important topic1", subtitle : "Foobar, foobar, foobar, foobar, foobar, foobar, foobar.", topics : rand(), posts : rand(), nickname : "Legonian", timestamp : "Mar 12, 2017"  },
-    { suptitle : "Some very important topic2", subtitle : "Foobar, foobar, foobar, foobar, foobar, foobar, foobar.", topics : rand(), posts : rand(), nickname : "Legonian", timestamp : "Mar 12, 2017"  },
-    { suptitle : "Some very important topic3", subtitle : "Foobar, foobar, foobar, foobar, foobar, foobar, foobar.", topics : rand(), posts : rand(), nickname : "Legonian", timestamp : "Mar 12, 2017"  },
-    { suptitle : "Some very important topic4", subtitle : "Foobar, foobar, foobar, foobar, foobar, foobar, foobar.", topics : rand(), posts : rand(), nickname : "Legonian", timestamp : "Mar 12, 2017"  },
-    { suptitle : "Some very important topic5", subtitle : "Foobar, foobar, foobar, foobar, foobar, foobar, foobar.", topics : rand(), posts : rand(), nickname : "Legonian", timestamp : "Mar 12, 2017"  }
-  ])
-  
   res.render('index')
 })
 
