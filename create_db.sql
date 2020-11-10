3bd8f5658e0d471a2bee71bdc4534ba8308cc3c4
Oleh <oleg.2014@gmail.com>
2020-11-10 07:44:14 +0200

    change sql script: add email and last name

--- create_db.sql
+++ create_db.sql
@@ -5,13 +5,15 @@ CREATE TABLE users(
   user_id int primary key generated always as identity,
   username varchar(20) not null UNIQUE,
   hash varchar(60) not null,
+  email varchar(60) not null,
   first_name varchar(30) not null,
+  last_name varchar(30),
   rating int not null default 0 check (0 <= rating),
   avatar bytea,
   user_info varchar(420),
   posts_count int not null default 0 check (0 <= posts_count),
   create_date timestamp not null default current_timestamp,
-  country varchar(20)
+  country varchar(30) not null
 );
 
 CREATE TABLE posts(
