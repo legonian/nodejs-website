548287db4f932975a62c4ca881c513e294c6361a
Oleh <oleg.2014@gmail.com>
2020-11-10 07:48:08 +0200

    disable spellchecker and downloading of fonts

--- public/javascripts/newpost.js
+++ public/javascripts/newpost.js
@@ -1,4 +1,6 @@
 const simplemde = new window.SimpleMDE({
-  element: document.getElementById('postBody')
+  element: document.getElementById('postBody'),
+  autoDownloadFontAwesome: false,
+  spellChecker: false
 })
 simplemde.value()
